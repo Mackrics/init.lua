ddde9e250e202374417320cd0cb6aedcf6bdf9b9
Markus Ljungqvist <Markus@Ljungqvist.xyz>
2023-05-12 19:32:52 +0200

    transparent background, refactor

--- lua/config.lua
+++ lua/config.lua
@@ -106,3 +106,46 @@ require'lspconfig'.ltex.setup{
 vim.diagnostic.config {
   virtual_text = false,
 }
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = true,
+    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+vim.cmd.colorscheme "catppuccin"
+vim.cmd('highlight ColorColumn ctermbg=black')
+vim.cmd('highlight CursorLine cterm=none ctermbg=none ctermfg=none guibg=none guifg=none')
+vim.cmd('highlight CursorLineNr guifg=#FAE3B0')
