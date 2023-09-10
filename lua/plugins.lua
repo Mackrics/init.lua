e429df68870115f1faa760243dcf493a32b7ba70
Markus <hello@mackrics.com>
2023-09-10 16:03:25 +0200

    updated neovim config to use lazy instead of packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,59 +1,71 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-  -- Send stuff to terminal
-  use 'jpalardy/vim-slime'
 
-  -- Manage files
-  use 'preservim/nerdtree'
 
-  -- dependency of a lot of packages
-  use 'nvim-lua/plenary.nvim'
-
-  -- File search
-  use 'nvim-telescope/telescope.nvim'
-
-  -- Color theme
-  use 'catppuccin/nvim'
-
+local plugins = {
+ 
+   -- Send stuff to terminal
+  "jpalardy/vim-slime",
+ 
+   -- Manage files
+  "preservim/nerdtree",
+ 
+   -- dependency of a lot of packages
+  "nvim-lua/plenary.nvim",
+ 
+   -- File search
+  "nvim-telescope/telescope.nvim",
+ 
+   -- Color theme
+  "catppuccin/nvim",
+ 
   -- Pandoc syntax highlightning
-  use 'vim-pandoc/vim-pandoc-syntax'
-
-  -- Easily change sessions
-  use 'Shatur/neovim-session-manager'
-
-  use 'stevearc/dressing.nvim'
-
-  -- Quarto syntax highlightning
-  use 'quarto-dev/quarto-vim'
-
-  -- Custom line
-  use 'nvim-lualine/lualine.nvim'
-
-  -- Git plugin
-  use 'tpope/vim-fugitive'
-
-  -- Use snippets
-  use "L3MON4D3/LuaSnip"
-
-  -- Autocomplete
-  use 'hrsh7th/nvim-cmp'
-	
-  -- Autocomplete Directories
-  use 'hrsh7th/cmp-path'
-
-  -- Autocomplete Languageserver
-  use 'hrsh7th/cmp-nvim-lsp'
-
- -- LSP configuration
-  use 'neovim/nvim-lspconfig'
-
-  -- Autocomplete snippets
-  use 'saadparwaiz1/cmp_luasnip'
-
-  -- zen mode focused coding
-  use 'folke/zen-mode.nvim'
-
-end)
-
+  "vim-pandoc/vim-pandoc-syntax",
+ 
+   -- Easily change sessions
+  "Shatur/neovim-session-manager",
+ 
+  "stevearc/dressing.nvim",
+ 
+   -- Quarto syntax highlightning
+  "quarto-dev/quarto-vim",
+ 
+   -- Custom line
+  "nvim-lualine/lualine.nvim",
+ 
+   -- Git plugin
+  "tpope/vim-fugitive",
+ 
+   -- Use snippets
+  "L3MON4D3/LuaSnip",
+ 
+   -- Autocomplete
+  "hrsh7th/nvim-cmp",
+ 	
+   -- Autocomplete Directories
+  "hrsh7th/cmp-path",
+ 
+   -- Autocomplete Languageserver
+  "hrsh7th/cmp-nvim-lsp",
+ 
+  -- LSP configuration
+  "neovim/nvim-lspconfig",
+ 
+   -- Autocomplete snippets
+  "saadparwaiz1/cmp_luasnip",
+ 
+   -- zen mode focused coding
+   "folke/zen-mode.nvim"
+}
+require("lazy").setup(plugins, opts)
