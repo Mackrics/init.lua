37915da6b32c6a40317dc275663e376d19895733
Markus <Hello@Mackrics.com>
2024-10-19 11:48:19 +0200

    feat: add julia and typst

--- lua/treesitter-conf.lua
+++ lua/treesitter-conf.lua
@@ -1,6 +1,17 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "r", "lua", "python", "markdown", "csv", "markdown_inline", "go"},
+  ensure_installed = { 
+    "r",
+    "lua",
+    "python",
+    "markdown",
+    "csv",
+    "markdown_inline",
+    "go",
+    "rust",
+    "julia",
+    "typst"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
